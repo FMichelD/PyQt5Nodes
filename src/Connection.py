37643f7e42a1650f1eb5ec62170f19d896986fd8
FMichelD <fmicheld@gmail.com>
2017-11-27 18:44:45 -0200

    Paired with paceholder

--- src/Connection.py
+++ src/Connection.py
@@ -1,54 +1,37 @@
  # -*- coding: utf-8 -*-
 # !/usr/bin/env python3
 
-
 from PyQt5.QtCore import QObject, QUuid, pyqtSignal
 
-#from Connection import *
 from Serializable import *
-from Node import *
 from PortType import *
 from ConnectionGeometry import *
 from ConnectionState import *
 from NodeGraphicsObject import *
 from ConnectionGraphicsObject import *
 
-# namespace std
-# {
-# template<>
-# struct hash<QUuid>
-# {
-#   inline
-#   std::size_t
-#   operator()(QUuid const& uid) const
-#   {
-#     return qHash(uid);
-#   }
-# };
-# }
-
 ##----------------------------------------------------------------------------
 class Connection(QObject, Serializable):
-    
     def __init__(self, *args):
-        QObject.__init__(self)  
-  
+        QObject.__init__(self)
+
+        self._id = None
         self._inNode = None
         self._outNode = None
+        self._outPortIndex = INVALID
+        self._inPortIndex = INVALID
+        self._connectionState = ConnectionState()
         self._connectionGeometry = ConnectionGeometry()
-        #_connectionGraphicsObject = ConnectionGraphicsObject()    
-        #Node = Node()
 
         # method overload
         signature = tuple(arg.__class__ for arg in args)
-        
+
         import Node
         NodeCls = Node.Node
 
         typemap = {(PortType, NodeCls, PortIndex) : self.connectionPorts,
                     (Node, PortIndex, NodeCls, PortIndex) : self.connectionNodes }
-    
-       
+
         if signature in typemap:
             return typemap[signature](*args)
         else:
@@ -57,10 +40,11 @@ class Connection(QObject, Serializable):
     #-------------------------------------------------------------------------
     def connectionPorts(self, portType, node, portIndex):
         self._id = QUuid.createUuid()
-        self._outPortIndex = PortType.INVALID
-        self._inPortIndex = PortType.INVALID
-        self._connectionState = ConnectionState() 
-        
+
+        self._outPortIndex = INVALID
+        self._inPortIndex = INVALID
+        self._connectionState = ConnectionState()
+
         self.setNodeToPort(node, portType, portIndex)
         self.setRequiredPort(oppositePort(portType))
 
@@ -68,53 +52,41 @@ class Connection(QObject, Serializable):
     def connectionNodes(self, nodeIn, PortIndexIn, nodeOut, portIndexOut):
         self._id = QUuid.createUuid()
 
-        
         self._outNode = Node(nodeOut)
         self._inNode = Node(nodeIn)
         self._outPortIndex = portIndexOut
         self._inPortIndex = portIndexIn
         self._connectionState = ConnectionState()
-        
+
         self.setNodeToPort(nodeIn, portType.In, portIndexIn)
         self.setNodeToPort(nodeOut, portType.Out, portIndexOut)
 
     #-------------------------------------------------------------------------
     def __del__(self):
-#        curframe = inspect.currentframe()
-#        calframe = inspect.getouterframes(curframe, 2)
-#        print('caller name:', calframe[1][3])
-#        print('on:', calframe[1][1])
-
-
         self.propagateEmptyData()
 
-        if(not self._inNode is None):
-
+        if(self._inNode):
             self._inNode.nodeGraphicsObject().update()
 
-        if(not self._outNode is None):
-
+        if(self._outNode):
             self._outNode.nodeGraphicsObject().update()
 
-#        print("Connection destructor")
+        print("Connection {} destructor".format(self._id))
 
     #-------------------------------------------------------------------------
     def save(self) -> dict:
-
         connectionJson = dict()
 
-        if( (not self._inNode is None) and (not self._outNode is None) ):
+        if(self._inNode and self._outNode):
 
             connectionJson["in_id"] = self._inNode.id().toString()
-
             connectionJson["in_index"] = self._inPortIndex
 
             connectionJson["out_id"] = self._outNode.id().toString()
-
             connectionJson["out_index"] = self._outPortIndex
 
         return connectionJson
-  
+
     #-------------------------------------------------------------------------
     def id(self) -> QUuid:
         return self._id
@@ -124,19 +96,12 @@ class Connection(QObject, Serializable):
         self._connectionState.setRequiredPort(dragging)
 
         if(dragging == PortType.Out):
-
             self._outNode = None
-
             self._outPortIndex = INVALID
-
         elif(dragging == PortType.In):
-
             self._inNode = None
-
             self._inPortIndex = INVALID
-
         else:
-
             Q_UNREACHABLE();
 
     #-------------------------------------------------------------------------
@@ -144,11 +109,7 @@ class Connection(QObject, Serializable):
         return self._connectionState.requiredPort()
 
     #-------------------------------------------------------------------------
-    def setGraphicsObject(self, graphics: ConnectionGraphicsObject):        
-        curframe = inspect.currentframe()
-        calframe = inspect.getouterframes(curframe, 2)
-        #print('caller name:', calframe[1][3])
-
+    def setGraphicsObject(self, graphics: ConnectionGraphicsObject):
         self._connectionGraphicsObject = graphics
 
         # // This function is only called when the ConnectionGraphicsObject
@@ -178,35 +139,22 @@ class Connection(QObject, Serializable):
 
     #-------------------------------------------------------------------------
     def getPortIndex(self, portType):
-
-#        result = INVALID
-
         if(portType == PortType.In):
-
             return self._inPortIndex
-
         elif(portType == PortType.Out):
-
             return self._outPortIndex
-
         else:
-
-            Q_UNREACHABLE();        
+            return INVALID
 
     #-------------------------------------------------------------------------
     def setNodeToPort(self, node, portType, portIndex):
-
-#        nodeWeak = self.getNode(portType)
-#        nodeWeak = node
-        
-        if(portType == PortType.In):            
-            self._inNode = node        
-        elif(portType == PortType.Out):            
-            self._outNode = node        
-        else:            
+        if(portType == PortType.In):
+            self._inNode = node
+        elif(portType == PortType.Out):
+            self._outNode = node
+        else:
             Q_UNREACHABLE()
-        
-        
+
         if(portType == PortType.Out):
             self._outPortIndex = portIndex
         elif(portType == PortType.In):
@@ -228,7 +176,7 @@ class Connection(QObject, Serializable):
 
     #-------------------------------------------------------------------------
     def getConnectionGraphicsObject(self):
-        return self._connectionGraphicsObject       
+        return self._connectionGraphicsObject
 
     #-------------------------------------------------------------------------
     def connectionState(self):
@@ -245,45 +193,39 @@ class Connection(QObject, Serializable):
         elif(portType == PortType.Out):
             return self._outNode
         else:
-            Q_UNREACHABLE();
+            return None
 
     #-------------------------------------------------------------------------
     def clearNode(self, portType):
-        self.getNode(portType)# = None
-
         if(portType == PortType.In):
+            self._inNode = None
             self._inPortIndex = INVALID
         elif(portType == PortType.Out):
+            self._outNode = None
             self._outPortIndex = INVALID
-        else:
-            Q_UNREACHABLE();
+
 
     #-------------------------------------------------------------------------
     def dataType(self):
-
         if(self._inNode):
-            #print("_inNode")
             index = self._inPortIndex
             portType = PortType.In
             validNode = self._inNode
         elif(self._outNode):
-            #print("_outNode")
             index = self._outPortIndex
             portType = PortType.Out
             validNode = self._outNode
-        else:
-            Q_UNREACHABLE();
 
-        if(not validNode is None):
+        if(validNode):
             model = validNode.nodeDataModel()
-            
+
             return model.dataType(portType, index)
 
         Q_UNREACHABLE();
 
     #-------------------------------------------------------------------------
     def propagateData(self, nodeData):
-        if(not self._inNode is None):
+        if(self._inNode):
             self._inNode.propagateData(nodeData, self._inPortIndex)
 
     #-------------------------------------------------------------------------
@@ -293,13 +235,10 @@ class Connection(QObject, Serializable):
 
     #-------------------------------------------------------------------------
     #signals
+    updated = pyqtSignal(object,  name="updatedConn")
 
-    #    def updated(self):
-    #    Connection = None
-    updated = pyqtSignal(object,  name="Connection")
-#        updated.emit(self)
 
 ##----------------------------------------------------------------------------
 
 
- 
+
