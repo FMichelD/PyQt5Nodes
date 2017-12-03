d2a926d659fc9be738871ec65524d74227536137
FMichelD <FMichelD@gmail.com>
2017-12-03 01:21:56 -0200

    added decimal to integer converter

---
+++ examples/calculator/DecimalToIntegerModel.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python3
+
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+import sys
+sys.path.insert(0, '/home/fmicheld/Workspace/PyQt/PyQt5Nodes/')
+from PyQt5Nodes.NodesDataModel import *
+from PyQt5Nodes.NodesData import *
+from PyQt5Nodes.PortType import *
+
+from DecimalData import *
+from IntegerData import *
+
+class DecimalToIntegerModel(NodesDataModel):
+    def __init__(self):
+        self._decimal = DecimalData()
+        self._integer = IntegerData()
+        
+    #--------------------------------------------------------------------------
+    #override
+    def caption(self):
+        return "Decimal to integer"
+    
+    #--------------------------------------------------------------------------
+    #override
+    def captionVisible(self):
+        return True
+    
+    #--------------------------------------------------------------------------
+    #override
+    def name(self):
+        return "DecimalToInteger"
+    
+    #--------------------------------------------------------------------------
+    #override
+    def clone(self):
+        return DecimalToIntegerModel() 
+    
+    #--------------------------------------------------------------------------
+    #override
+    def save(self):
+        modelJson = dict()
+        
+        modelJson["name"] = name()
+        
+        return modelJson
+    
+    #--------------------------------------------------------------------------
+    #override
+    def nPorts(self, portType: PortType):
+        result = 1
+        
+        if(portType == PortType.In):
+            result = 1
+        elif(portIndex == PortType.Out):
+            result = 1
+        
+        return result
+        
+    #--------------------------------------------------------------------------
+    #override
+    def dataType(self, portType: PortType, portIndex: PortIndex):
+        if(portType == PortType.In):
+            return DecimalData().type()
+        
+        return IntegerData().type()
+    
+    #--------------------------------------------------------------------------
+    #override
+    def outData(self, port: PortIndex):
+        return self._integer
+    
+    #--------------------------------------------------------------------------
+    #override
+    def setInData(self, data: NodeData, portIndex: PortIndex):
+        numberData = DecimalData(data)
+        
+        if(portIndex == 0):
+            self._decimal = numberData
+        
+        if(self._decimal):
+            self._integer = IntegerData(self._decimal.number())
+            
+        outPortIndex = 0
+        
+        dataUpdated.emit(outPortIndex)
+    
+    #--------------------------------------------------------------------------
+    #override
+    def embeddedWidget(self):
+        return None
