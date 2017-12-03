07e6beba30000dc878817137b158e518f0c1dc5d
FMichelD <FMichelD@gmail.com>
2017-12-03 01:29:33 -0200

    added class to represent integer data

---
+++ examples/calculator/IntegerData.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python3
+
+import sys
+sys.path.insert(0, '/home/fmicheld/Workspace/PyQt/PyQt5Nodes/')
+from PyQt5Nodes.NodeDataModel import *
+from PyQt5Nodes.NodeData import *
+
+class IntegerData(NodeData):
+    def __init__(self,  number:float=0.0):
+        super().__init__(self)
+        
+        self._number = number
+    
+    #--------------------------------------------------------------------------
+    #override
+    def type(self):
+        NodeDataType.id = "integer"
+        NodeDataType.name = "Integer"
+        
+        return NodeDataType
+    
+    #--------------------------------------------------------------------------
+    def number(self):
+        return self._number
+    
+    #--------------------------------------------------------------------------
+    def numberAsText(self):
+        return str(self._number) 
+    
